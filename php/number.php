39e8d50678ea9600e4924bc14a5bc0237cc45369
ZZWILLIAMXX <49970753+ZZWILLIAMXXTrue@users.noreply.github.com>
2021-12-21 09:48:36 -0600

    Math

---
+++ php/number.php
@@ -0,0 +1,37 @@
+<?php
+  /*
+  WARNINg: This includes math!
+  *\
+  
+  $num = 10;
+  
+  // Addition
+  $num += 2; # 10+2
+  echo $num; # Prints 12
+  echo $num + 2; # Prints 14, doesn't affect the $num variable
+  
+  //Subtraction
+  $num -= 2; # 12-2
+  echo $num; # Prints 10
+  echo $num - 2; # Prints 8, doesn't affect the $num variable
+
+  //Division
+  $num /= 2; # 10/2
+  echo $num; # Prints 5
+  echo $num / 2; # Prints 2.5, doesn't affect the $num variable
+  
+  //Multiplication
+  $num *= 2; # 5*2
+  echo $num; # Prints 10
+  echo $num * 2; # Prints 20, doesn't affect the $num variable
+  
+  //Modulo
+  $num += 1; # 10+1=11
+  $num %= 2; # 11%2
+  echo $num; # Prints 1 because one is how much is left
+  echo $num + 4 % 3; # Prints 2, doesn't affect the $num variable
+  
+  //Exponentiation
+  $num = 1**2; # 1*1
+  echo $num; # Prints 1
+  echo $num ** 2; # Prints 1, doesn't affect the $num variable
